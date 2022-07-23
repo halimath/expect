c79cf4aca492c607a74730c66fba7031f5e421f9
Alexander Metzner <alexander.metzner@gmail.com>
2022-07-23 14:07:46 +0200

    fix: use matcher.T also for assert.That

--- assert/that.go
+++ assert/that.go
@@ -1,16 +1,14 @@
 package assert
 
 import (
-	"testing"
-
 	"github.com/halimath/assertthat-go/matcher"
 )
 
-// That implements the entry function to apply a set of matchers on a given value.
-// That accepts the testing interface, to given value and a variable list of matchers
-// to apply. That calls each matchers Match method in the order the matchers are given.
-// Thus, using t.Fatal in one matcher will prevent the following matchers from running.
-func That[G any](t testing.TB, got G, matcher ...matcher.Matcher[G]) {
+// That implements the entry function to apply a set of matchers on a given value. That accepts the testing
+// interface, the given value and a variable list of matchers to apply. That calls each matchers Match method
+// in the order the matchers are given. Thus, using t.Fatal in one matcher will prevent the following matchers
+// from running.
+func That[G any](t matcher.T, got G, matcher ...matcher.Matcher[G]) {
 	for _, m := range matcher {
 		m.Match(t, got)
 	}
