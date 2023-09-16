95e266f8b2b9391557180e45061b8f330b4c0a1f
Alexander Metzner <alexander.metzner@gmail.com>
2023-09-16 09:20:25 +0200

    boyscout: remove commented code

--- expect.go
+++ expect.go
@@ -3,20 +3,6 @@
 // custom assertions/expectations by implementing the Matcher interface.
 package expect
 
-// // Context defines the context used by Matcher to interact with the running test (i.e. failing it.). Using the
-// // Context interface Matchers can reject values with a custom message which will fail the test either
-// // immediately or deferred, depending on the surrounding call.
-// type Context interface {
-// 	// Fail fails the test producing a message using the provided args.
-// 	Fail(args ...any)
-
-// 	// Failf fails the test producing a message by formatting args according to format.
-// 	Failf(format string, args ...any)
-
-// 	// T provides access to the underlyings testing.TB.
-// 	T() TB
-// }
-
 // Matcher defines the interface implemented to execute a single expectation.
 type Matcher[G any] interface {
 	// Match is called with a Context interface and the given value got. Implementations should perform
@@ -56,39 +42,6 @@ type TB interface {
 	TempDir() string
 }
 
-type failFunc func()
-
-// // context implements Context. It calls fail to markt the test as failed.
-// type context struct {
-// 	t    TB
-// 	fail failFunc
-// }
-
-// var _ Context = &context{}
-
-// func (ctx *context) Fail(args ...any) {
-// 	ctx.t.Helper()
-// 	msg := fmt.Sprint(args...)
-// 	ctx.t.Log(msg)
-// 	ctx.fail()
-// }
-
-// func (ctx *context) Failf(format string, args ...any) {
-// 	ctx.t.Helper()
-// 	ctx.Fail(fmt.Sprintf(format, args...))
-// }
-
-// func (ctx *context) T() TB {
-// 	return ctx.t
-// }
-
-// // Chain defines an intermediate type used to chain expectations on a single type. Normally, test code will
-// // not use this type but instead call the chaining methods to invoce Matchers.
-// type Chain[T any] struct {
-// 	got T
-// 	ctx Context
-// }
-
 // ExpectThat starts a new expectation chain using got as the value to expect things from. It uses t to interact
 // with the running test. Any failing matches originating from a call to ExpectThat cause the test to fail at
 // the end of the execution; it works like caling t.Error(...).
@@ -119,21 +72,3 @@ type ensureTB struct {
 func (t *ensureTB) Error(args ...any)                 { t.Helper(); t.Fatal(args...) }
 func (t *ensureTB) Errorf(format string, args ...any) { t.Helper(); t.Fatalf(format, args...) }
 func (t *ensureTB) Fail()                             { t.Helper(); t.FailNow() }
-
-// // Is adds m to e providing a fluent API.
-// func (e *Chain[T]) Is(m Matcher[T]) *Chain[T] { e.ctx.T().Helper(); return e.runMatcher(m) }
-
-// // Has adds m to e providing a fluent API.
-// func (e *Chain[T]) Has(m Matcher[T]) *Chain[T] { e.ctx.T().Helper(); return e.runMatcher(m) }
-
-// // And adds m to e providing a fluent API.
-// func (e *Chain[T]) And(m Matcher[T]) *Chain[T] { e.ctx.T().Helper(); return e.runMatcher(m) }
-
-// // Matches adds m to e providing a fluent API.
-// func (e *Chain[T]) Matches(m Matcher[T]) *Chain[T] { e.ctx.T().Helper(); return e.runMatcher(m) }
-
-// func (e *Chain[T]) runMatcher(m Matcher[T]) *Chain[T] {
-// 	e.ctx.T().Helper()
-// 	m.Match(e.ctx, e.got)
-// 	return e
-// }
